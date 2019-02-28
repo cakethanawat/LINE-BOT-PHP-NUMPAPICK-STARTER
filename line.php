0b4738ab2a7b641e4da636edb2371f1dc1f22981
cakethanawat <48070609+cakethanawat@users.noreply.github.com>
2019-02-28 13:05:59 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'uZu3rsewKmQ4n4UQauWw2JYZQYL/WKiODoTHK43Yrqo126JKT32dW37jOS0sMpFTQJtNvUsIFLXF+YX84g9E9rMYbx6R+c7FjP940fRuxYIRJPSpIIoDKgA1B6FQGtn3zAfxE4fuHF3fIMpds07NTwdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U8f400f332bad6b28e4503c6ee4a055b6',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
